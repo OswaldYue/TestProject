0170d09ad81205f26c5bdec51a6781273dcec96c
mingongwei <mdx918@163.com>
2019-05-06 20:01:46 +0800

    [提交人] :闵功伟 [提交时间]:2019-05-06 [提交明细]:netty测试代码v1 [是否自测]:自测完毕

---
+++ netty-lecture-01/src/main/java/com/mgw/netty/firstexample/TestServer.java
@@ -0,0 +1,40 @@
+package com.mgw.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) throws Exception{
+
+        // 定义两个事件循环组 其实就是线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).
+                    childHandler(new TestServerInitializer());
+
+            ChannelFuture sync = serverBootstrap.bind(8899).sync();
+
+
+            sync.channel().closeFuture().sync();
+        }catch (Exception e) {
+
+            e.printStackTrace();
+
+        }finally {
+
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+
+        }
+
+    }
+
+}
