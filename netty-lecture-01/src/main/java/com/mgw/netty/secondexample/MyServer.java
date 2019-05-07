8ff6d4e1ada49250383c524ead78bc6e3a051720
mingongwei <mdx918@163.com>
2019-05-07 08:59:59 +0800

    [提交人] :闵功伟 [提交时间]:2019-05-07 [提交明细]:netty测试代码v1 [是否自测]:自测完毕

--- netty-lecture-01/src/main/java/com/mgw/netty/secondexample/MyServer.java
+++ netty-lecture-01/src/main/java/com/mgw/netty/secondexample/MyServer.java
@@ -1,7 +1,42 @@
 package com.mgw.netty.secondexample;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 2.netty用来socket客户端与服务端
+ * */
 public class MyServer {
 
-    
+    public static void main(String[] args) throws Exception{
+
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boosGroup,workerGroup).channel(NioServerSocketChannel.class).
+                    childHandler(null);
+
+            ChannelFuture channelFuture = bootstrap.bind(8899).sync();
+
+            channelFuture.channel().closeFuture().sync();
+
+
+        }finally {
+
+            boosGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+
+        }
+
+
+
+
+    }
+
 
 }
