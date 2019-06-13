24ab5c475cff22ed2c6ae6eb6a73a62b7bb37f15
mingongwei <mdx918@163.com>
2019-06-13 19:37:47 +0800

    [提交人] :闵功伟 [提交时间]:2019-06-13 [提交明细]:测试项目提交 [是否自测]:自测完毕

--- java8/java8-test/src/main/java/com/mgw/FunctionTest.java
+++ java8/java8-test/src/main/java/com/mgw/FunctionTest.java
@@ -9,19 +9,37 @@ public class FunctionTest {
 
         FunctionTest test = new FunctionTest();
 
-        System.out.println(test.compute(1,value -> {
+        System.out.println(test.compute(1,value -> { return 2 * value;}));
+        System.out.println(test.compute(2,value -> value * value));
 
-            return 2 * value;
+        System.out.println(test.convent(3,value -> { return value + "aaaaaa"; }));
 
-        }));
 
+        System.out.println(test.compose(2,value-> 3 * value,value -> value * value));
+        System.out.println(test.andThen(2,value -> 3 * value, value -> value * value));
     }
 
 
     public int compute(int a, Function<Integer,Integer> function) {
+        return function.apply(a);
+    }
+
+    public String convent(int a,Function<Integer,String> function) {
 
         return function.apply(a);
 
+    }
+
+
+    public int compose(int a ,Function<Integer,Integer> function1,Function<Integer,Integer> function2) {
+
+        return function1.compose(function2).apply(a);
+    }
+
+    public int andThen(int a ,Function<Integer,Integer> function1,Function<Integer,Integer> function2) {
+
+
+        return function1.andThen(function2).apply(a);
     }
 
 
