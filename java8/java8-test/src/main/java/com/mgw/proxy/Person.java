3fae7486f94298bd3f34562db70af07c083a8153
mgw <979530760@qq.com>
2019-06-30 09:24:06 +0800

    测试

---
+++ java8/java8-test/src/main/java/com/mgw/proxy/Person.java
@@ -0,0 +1,11 @@
+package com.mgw.proxy;
+
+public class Person implements Subject {
+
+    @Override
+    public void shopping() {
+        System.out.println("付款，买到想要的商品");
+    }
+
+
+}
--- java8/java8-test/src/main/java/com/mgw/proxy/TestProxy.java
+++ java8/java8-test/src/main/java/com/mgw/proxy/TestProxy.java
@@ -1,30 +1,32 @@
 package com.mgw.proxy;
 
+import org.junit.Test;
+
 public class TestProxy {
 
-//    @Test
-//    public void testProxy() {
-//
-//        //创建代理对象的对象
-//        CreateProxy createProxy = new CreateProxy();
-//        Person person = new Person();
-//
-//        //代理对象
-//        Subject proxy = (Subject)createProxy.create(person);
-//        //代理对象调用方法
-//        proxy.shopping();
-//
-//    }
-//
-//    public static void main(String[] args) {
-//        //创建代理对象的对象
-//        CreateProxy createProxy = new CreateProxy();
-//        Person person = new Person();
-//
-//        //代理对象
-//        Subject proxy = (Subject)createProxy.create(person);
-//        //代理对象调用方法
-//        proxy.shopping();
-//    }
+    @Test
+    public void testProxy() {
+
+        //创建代理对象的对象
+        CreateProxy createProxy = new CreateProxy();
+        Person person = new Person();
+
+        //代理对象
+        Subject proxy = (Subject)createProxy.create(person);
+        //代理对象调用方法
+        proxy.shopping();
+
+    }
+
+    public static void main(String[] args) {
+        //创建代理对象的对象
+        CreateProxy createProxy = new CreateProxy();
+        Person person = new Person();
+
+        //代理对象
+        Subject proxy = (Subject)createProxy.create(person);
+        //代理对象调用方法
+        proxy.shopping();
+    }
 
 }
