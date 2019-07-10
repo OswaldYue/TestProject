20ec2b0dcbc195bd1dcbd814e33a91fcad248279
mingongwei <mdx918@163.com>
2019-07-10 14:22:59 +0800

    测试

--- java8/java8-test/src/main/java/com/mgw/aop/LogAspect.java
+++ java8/java8-test/src/main/java/com/mgw/aop/LogAspect.java
@@ -1,8 +1,13 @@
 package com.mgw.aop;
 
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 
 /***
@@ -20,6 +25,8 @@ import org.springframework.stereotype.Component;
  *
  *      @After
  * }
+ *  正常执行: @Before(前置通知) === @After(后置通知) === @AfterReturning(正常返回)
+ *  异常执行: @Before(前置通知) === @After(后置通知) === @AfterThrowing(异常通知)
  *
  *
  * */
@@ -31,29 +38,100 @@ public class LogAspect {
     @Pointcut("execution(* com.mgw.aop.MyCaculator.*(..))")
     public void point() {}
 
+    /**
+     * JoinPoint封装了详细信息
+     *
+     * */
     @Before("point()")
-    public void logBefore() {
+    public void logBefore(JoinPoint joinPoint) {
 
-        System.out.println("[xxx]方法开始执行");
+        Object[] args = joinPoint.getArgs();
+
+        //获取方法签名
+        Signature signature = joinPoint.getSignature();
+
+        System.out.println("LogAspect切面前置["+signature.getName()+"]方法开始执行,参数列表是:"+ Arrays.asList(args));
 
     }
 
     @After("point()")
-    public void logAfter() {
+    public void logAfter(JoinPoint joinPoint) {
 
-        System.out.println("[xxx]方法最终结束");
+        Signature signature = joinPoint.getSignature();
+
+        System.out.println("LogAspect切面后置["+signature.getName()+"]方法最终结束");
 
     }
 
-    @AfterReturning("point()")
-    public void logReturn() {
-        System.out.println("[xxx]方法正常执行完成,结果是:");
+    /**
+     * 需要告诉spring那个参数来接受返回值
+     * */
+    @AfterReturning(value = "point()",returning = "result")
+    public void logReturn(JoinPoint joinPoint,Object result) {
+
+        Signature signature = joinPoint.getSignature();
+
+
+        System.out.println("LogAspect切面返回["+signature.getName()+"]方法正常执行完成,结果是:"+result);
     }
 
-    @AfterThrowing("point()")
-    public void logException() {
+    @AfterThrowing(value = "point()",throwing = "e")
+    public void logException(JoinPoint joinPoint,Exception e) {
 
-        System.out.println("[xxx]方法发生异常,异常信息是:");
+        Signature signature = joinPoint.getSignature();
+
+        System.out.println("LogAspect切面异常["+signature.getName()+"]方法发生异常,异常信息是:"+e.getMessage());
+    }
+
+    /**
+     * 环绕通知:是spring中最强大的通知
+     * 其实就是动态代理
+     *
+     * 环绕通知是优于普通通知执行的
+     * 普通前置 ===> {
+     *              try {
+     *
+     *                  环绕前置
+     *                  (环绕执行)目标方法运行
+     *                  环绕返回
+     *
+     *              }catch() {
+     *
+     *                  环绕异常
+     *
+     *              }finally{
+     *
+     *                  环绕后置
+     *
+     *              }
+     *
+     *              } ===> 普通后置 ===> 普通返回/普通异常
+     *
+     * 注意:普通前置与环绕前置的执行顺序可能不确定
+     * */
+    @Around(value = "point()")
+    public Object myAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        Object[] args = proceedingJoinPoint.getArgs();
+        Signature signature = proceedingJoinPoint.getSignature();
+        Object result = null;
+
+        try {
+
+            //实际上就是method.invoke(obj,args)
+            System.out.println("LogAspect切面环绕[环绕前置通知]"+signature.getName() +"方法开始");
+            result = proceedingJoinPoint.proceed(args);
+            System.out.println("环绕...");
+            System.out.println("LogAspect切面环绕[环绕返回通知]"+signature.getName() +"方法返回,结果是:"+result);
+
+        }catch (Exception e) {
+            System.out.println("LogAspect切面环绕[环绕异常通知]"+signature.getName() +"方法异常,异常信息是:"+e.getMessage());
+            throw  new RuntimeException(e);
+
+        }finally {
+            System.out.println("LogAspect切面环绕[环绕后置通知]"+signature.getName() +"方法最终结束");
+        }
+        return result;
     }
 
 }
--- java8/java8-test/src/main/java/com/mgw/aop/Test.java
+++ java8/java8-test/src/main/java/com/mgw/aop/Test.java
@@ -55,17 +55,105 @@ public class Test {
         Caculator caculator = (Caculator)context.getBean("myCaculator");
         System.out.println(caculator);
 
+        bean.div(3,0);
 
+
+    }
+
+
+    public static void test4() {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+
+        //当MyCaculator没有接口时,会创建一个cglib代理
+        MyCaculator bean = context.getBean(MyCaculator.class);
+
+        //当MyCaculator没有接口时,会创建一个cglib代理,此时可以转为MyCaculator类型
+        MyCaculator bean2 = (MyCaculator)context.getBean("myCaculator");
+
+    }
+
+    public static void test5() {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+        Caculator bean = context.getBean(Caculator.class);
+
+        int add = bean.add(3, 2);
+        System.out.println("=========结果:"+add);
+        bean.div(3,0);
+    }
+
+    public static void test6() {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+        Caculator bean = context.getBean(Caculator.class);
+
+        /**
+         * 当无环绕通知时:
+         * =============================
+         * LogAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * ValidateAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * add 执行
+         * ValidateAspect切面后置[add]方法最终结束
+         * ValidateAspect切面返回[add]方法正常执行完成,结果是:5
+         * LogAspect切面后置[add]方法最终结束
+         * LogAspect切面返回[add]方法正常执行完成,结果是:5
+         * =========结果:5
+         *
+         *
+         * 当有环绕通知时:目标方法永远只执行一次(环绕先就在环绕中执行)
+         * =============================
+         * LogAspect切面环绕[环绕前置通知]add方法开始
+         * LogAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * ValidateAspect切面环绕[环绕前置通知]add方法开始
+         * ValidateAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * add 执行
+         * 环绕...
+         * ValidateAspect切面环绕[环绕返回通知]add方法返回,结果是:5
+         * ValidateAspect切面环绕[环绕后置通知]add方法最终结束
+         * ValidateAspect切面后置[add]方法最终结束
+         * ValidateAspect切面返回[add]方法正常执行完成,结果是:5
+         * 环绕...
+         * LogAspect切面环绕[环绕返回通知]add方法返回,结果是:5
+         * LogAspect切面环绕[环绕后置通知]add方法最终结束
+         * LogAspect切面后置[add]方法最终结束
+         * LogAspect切面返回[add]方法正常执行完成,结果是:5
+         * =========结果:5
+         *
+         * 当环绕通知处于第二执行的切面类时:目标方法永远只执行一次(普通通知先就在普通中执行)
+         * =============================
+         * LogAspect切面环绕[环绕前置通知]add方法开始
+         * LogAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * ValidateAspect切面前置[add]方法开始执行,参数列表是:[3, 2]
+         * add 执行
+         * ValidateAspect切面后置[add]方法最终结束
+         * ValidateAspect切面返回[add]方法正常执行完成,结果是:5
+         * 环绕...
+         * LogAspect切面环绕[环绕返回通知]add方法返回,结果是:5
+         * LogAspect切面环绕[环绕后置通知]add方法最终结束
+         * LogAspect切面后置[add]方法最终结束
+         * LogAspect切面返回[add]方法正常执行完成,结果是:5
+         * =========结果:5
+         *
+         * 当两个切面同时切入时,后进入的先执行
+         * */
+        int add = bean.add(3, 2);
+        System.out.println("=========结果:"+add);
+        bean.div(3,0);
     }
 
     public static void main(String[] args) {
 
-
 //        test1();
 //        System.out.println("=============================");
 //        test2();
-        System.out.println("=============================");
-        test3();
+//        System.out.println("=============================");
+//        test3();
+//        System.out.println("=============================");
+//        test5();
 
+        System.out.println("=============================");
+
+        test6();
     }
 }
